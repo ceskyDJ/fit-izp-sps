62a355e21b28856861ff6e8406ff90497d87df18
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2020-11-30 21:02:10 +0100

    Base of data loading

--- sps.c
+++ sps.c
@@ -11,17 +11,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * @def DEFAULT_DELIMITER Default delimiter for case user didn't set different
  */
 #define DEFAULT_DELIMITER " "
+/**
+ * Flag for the last item
+ */
+#define LAST_ITEM "LAST_ITEM"
 
 /**
  * @def streq(first, second) Check if first equals second
  */
 #define streq(first, second) (strcmp(first, second) == 0)
 
+/**
+ * @typedef Error information tells how some action ended
+ * @field error Did it end with error? (=> if true, something bad happened; otherwise the operation was successful)
+ * @field message Description message in case of error = true
+ */
+typedef struct errorInfo {
+    bool error;
+    char *message;
+} ErrorInfo;
 /**
  * Individual table cell
  * @field data Cell's content
@@ -51,9 +65,14 @@ typedef struct table {
 } Table;
 
 // Input/output functions
+ErrorInfo loadTableFromFile(Table * table, char *file);
+ErrorInfo loadRowFromFile(Row *row, char *file);
+ErrorInfo loadCellFromFile(Cell *cell, char *file);
 void writeErrorMessage(const char *message);
 
 int main(int argc, char **argv) {
+    ErrorInfo err = {.error = false};
+
     /* ARGUMENTS PARSING */
     // Valid arguments: ./sps [-d DELIMITERS] <CMD_SEQUENCE> <FILE>
     // Check arguments count
@@ -83,14 +102,59 @@ int main(int argc, char **argv) {
     skippedArgs += 1;
 
     // Get file from arguments
-    FILE *inputFile;
-    if ((inputFile = fopen(argv[skippedArgs], "r+")) == NULL) {
-        writeErrorMessage("Soubor predany ve vstupnich argumentech se nepodarilo otevrit.");
+    char *inputFile = argv[skippedArgs];
+
+    /* DATA PARSING */
+    Table table;
+    if ((err = loadTableFromFile(&table, inputFile)).error) {
+        writeErrorMessage(err.message);
 
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
+}
+
+/**
+ * Constructs table with data from a file
+ * @param table Where to save the table
+ * @param file The file with data for table
+ * @return Error information
+ */
+ErrorInfo loadTableFromFile(Table *table, char *file) {
+    ErrorInfo err = {.error = false};
+
+    return err;
+}
+
+/**
+ * Constructs row with data from a file
+ * @param row Where to save the row
+ * @param file The file with data for the row
+ * @return Error information or special state for the last row in the table
+ */
+ErrorInfo loadRowFromFile(Row *row, char *file) {
+    ErrorInfo err = {.error = false};
+
+    return err;
+}
+
+/**
+ * Constructs cell with data from a file
+ * @param cell Where to save the cell
+ * @param file The file with data for the row
+ * @return Error information or special state for the last cell in the row
+ */
+ErrorInfo loadCellFromFile(Cell *cell, char *file) {
+    ErrorInfo err = {.error = false};
+
+    FILE *source;
+    if ((source = fopen(file, "r")) == NULL) {
+        writeErrorMessage("Soubor predany ve vstupnich argumentech se nepodarilo otevrit.");
+
+    }
+
+    return err;
 }
 
 /**
