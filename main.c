7a9a9ff2fca1c6e15f8e360c476473d3116a57dc
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2020-11-30 18:25:35 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * SPS
+ *
+ * Simple spreadsheet editor
+ *
+ * @author Michal Šmahel <xsmahe01@stud.fit.vutbr.cz>
+ * @date November-December 2020
+ * @version 1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    return EXIT_SUCCESS;
+}
