c2a42abfb76657c2a19d64f766480b6dab80b6a9
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2020-11-30 19:43:24 +0100

    Getting delimiters

--- sps.c
+++ sps.c
@@ -10,12 +10,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * @def DEFAULT_DELIMITER Default delimiter for case user didn't set different
+ */
+#define DEFAULT_DELIMITER " "
+
+/**
+ * @def streq(first, second) Check if first equals second
+ */
+#define streq(first, second) (strcmp(first, second) == 0)
 
 // Input/output functions
 void writeErrorMessage(const char *message);
 
 int main(int argc, char **argv) {
     /* ARGUMENTS PARSING */
+    // Valid arguments: ./sps [-d DELIMITERS] <CMD_SEQUENCE> <FILE>
+    // Check arguments count
     if (argc < 3) {
         writeErrorMessage("Nedostatecny pocet vstupnich argumentu.");
 
@@ -24,6 +37,17 @@ int main(int argc, char **argv) {
         writeErrorMessage("Prekrocen maximalni pocet vstupnich argumentu.");
 
         return EXIT_FAILURE;
+    }
+
+    // Get delimiter from arguments
+    int skippedArgs = 1;
+    char **delimiters;
+    if (argc == 5 && streq(argv[skippedArgs], "-d")) {
+        delimiters = &argv[skippedArgs + 1];
+        skippedArgs += 2;
+    } else {
+        const char *DELIMITER = DEFAULT_DELIMITER;
+        delimiters = (char **) &DELIMITER;
     }
 
     return EXIT_SUCCESS;
